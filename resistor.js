5488a6c053bfdde7d6a49410cda11bc1c964e348
Steve Goldberg <steve@gaiagps.com>
2014-02-26 19:46:13 -0500

    Embed vincent on home page

--- resistor.js
+++ resistor.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
+	SC.initialize({
+	    client_id: "67f2822a454cafa2b800fc88cb5c0518",
+	  });
+	SC.oEmbed("http://soundcloud.com/resistorsings/vincent-van-gogh-1", {color: '#E6E6E6'}, document.getElementById('vincent'));
+	
 	$('#RE').toggle('slide', 1000, function(){
 		$('#SIS').toggle('slide', {direction: 'right'}, 1000, function(){
-			$('#TOR').toggle('slide', 1000);
+			$('#TOR').toggle('slide', 1000, function(){
+				$('#vincent').fadeTo(1000, 1);
+			});
 		});
 	});
 });
