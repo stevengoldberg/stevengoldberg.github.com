90c0fefa387c8fba3ceae37093ea9868f811ec7d
Steve Goldberg <steve@gaiagps.com>
2014-02-27 14:27:11 -0500

    Tweet widget homepage

--- resistor.js
+++ resistor.js
@@ -1,14 +1,37 @@
+//SC.initialize({
+//   client_id: "67f2822a454cafa2b800fc88cb5c0518",
+//  });
+
+var numTweets = 20;
+
+function handleTweets(tweets){
+    //var current = Math.floor(Math.random()*numTweets);
+	var current = 0;
+    var element = document.getElementById('tweet');
+	var initial = true;
+    $('#tweet ul li').append(tweets[current]);
+	current = (current + 1) % numTweets;
+	window.setInterval(function() {
+		$('#tweet ul').fadeOut(500, function() {
+			$('#tweet ul li').empty();
+			$('#tweet ul li').append(tweets[current]);
+			current = (current + 1) % numTweets;
+			$('#tweet ul').fadeIn(500);
+		});
+    }, 10000);
+}
+
 $(document).ready(function() {
-	SC.initialize({
-	    client_id: "67f2822a454cafa2b800fc88cb5c0518",
-	  });
-	SC.oEmbed("http://soundcloud.com/resistorsings/vincent-van-gogh-1", {color: '#E6E6E6'}, document.getElementById('vincent'));
 	
-	$('#RE').toggle('slide', 1000, function(){
-		$('#SIS').toggle('slide', {direction: 'right'}, 1000, function(){
-			$('#TOR').toggle('slide', 1000, function(){
-				$('#vincent').fadeTo(1000, 1);
+	$('#RE').fadeTo(1000, 1, function(){
+		$('#SIS').fadeTo(1000, 1, function(){
+			$('#TOR').fadeTo(1000, 1, function(){
+				//$('#vincent').fadeTo(1000, 1);
 			});
 		});
 	});
+//	SC.oEmbed("http://soundcloud.com/resistorsings/vincent-van-gogh-1", {color: '#E6E6E6'}, document.getElementById('vincent'));
+	
+	twitterFetcher.fetch('438885011689713665', 'tweet', numTweets, true, false, false, undefined, true, handleTweets);
+	
 });
