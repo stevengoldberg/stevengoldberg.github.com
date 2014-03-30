3341b675f1652834d537da239ce740691aae7beb
Steve Goldberg <steve@gaiagps.com>
2014-03-29 23:38:54 -0400

    Code refactoring

--- js/resistor.js
+++ js/resistor.js
@@ -1,30 +1,29 @@
+SC.initialize({										//Initialize soundcloud API
+   client_id: "67f2822a454cafa2b800fc88cb5c0518",
+   redirect_uri: 'http://resistorsings.com'
+});
+
 $(document).ready(function() {
-	window.scrollReveal = new scrollReveal();
-	$('#RE').fadeTo(1500, 1, function(){
+	numTweets = 20;
+	window.scrollReveal = new scrollReveal(); // Activate scrollReveal plugin for sliding element entrances
+	$('#RE').fadeTo(1000, 1, function(){	// Fade in the headline in 3 parts
 		$('#SIS').fadeTo(1000, 1, function(){
 			$('#TOR').fadeTo(1000, 1, function(){
 			});
 		});
 	});
-	twitterFetcher.fetch('438885011689713665', 'tweet', numTweets, true, false, true, undefined, true, handleTweets);
-	playSongs();
-	getPhotos();
-	showDates();
-	playerStyle();
+	twitterFetcher.fetch('438885011689713665', 'tweet', numTweets, true, false, true, undefined, true, handleTweets); // Print the tweets
+	playSongs(); // Load the music player
+	getPhotos(); // Load the photo viewer
+	showDates(); // Load the show dates 
 });
-
-SC.initialize({										//Initialize soundcloud API
-   client_id: "67f2822a454cafa2b800fc88cb5c0518",
-   redirect_uri: 'http://stevengoldberg.github.io'
-});
-
-var numTweets = 20;
 
 function handleTweets(tweets){
-    var currentTweet = 0;
-	splitTweet(tweets[currentTweet]);
-	var prevRotation = 0;
-	$('.prevTweet').click(function(e){
+    var currentTweet = 0,
+		prevRotation = 0,
+		nextRotation = 0;
+	splitTweet(tweets[currentTweet]); // Print the current tweet
+	$('.prevTweet').click(function(e){ // Rotate the knob, fade out the text, replace it with the previous tweet in the list
 		e.preventDefault();
 		prevRotation -= 40;
 		$('img.oscillator.prevTweet').css('transform','rotate(' + prevRotation + 'deg)');
@@ -34,8 +33,7 @@ function handleTweets(tweets){
 			$('#text').fadeIn(500);
 		});
 	});
-	var nextRotation = 0;
-	$('.nextTweet').click(function(e){
+	$('.nextTweet').click(function(e){ //Rotate the knob, fade out the text, replace it with the next tweet in the list
 		e.preventDefault();
 		nextRotation += 40;
 		$('img.oscillator.nextTweet').css('transform','rotate(' + nextRotation + 'deg)');
@@ -48,38 +46,33 @@ function handleTweets(tweets){
 }
 
 function splitTweet(tweet){
-	var end = tweet.length - 1;
-	var search = tweet.search('<p class="interact">');
-	var tweetText = tweet.slice(0,search);
-	var buttons = tweet.slice(search, end);	
-    $('#text').html(tweetText);
+	var end = tweet.length - 1,
+		search = tweet.search('<p class="interact">'), 
+		tweetText = tweet.slice(0,search),
+		buttons = tweet.slice(search, end);	
+    $('#text').html(tweetText); // Print the body of the tweet to the main panel and the interaction buttons to the switches
 	$('#buttons').html(buttons);
 }
 
 Track = function (trackId){
     var currentTrack = "";
-	
     SC.stream(trackId, function(sound){
         currentTrack = sound;
 	});
-
     this.play = function() {
         currentTrack.play({
-			onfinish: function(){
+			onfinish: function(){ // When a song ends, start the next song
         	$('.nextSong').click();
        	 	},
-			whileplaying: function(){
+			whileplaying: function(){ // While a song is playing, update the progress bar
 				var percentComplete = ((this.position / this.duration) * 100);
 				$('#progress').css('width', percentComplete + '%');
 			}
 		});
     };
-
     this.pause = function() {
         currentTrack.togglePause();
     };
-	
-
     this.stop = function() {
         currentTrack.stop();
     };
@@ -87,26 +80,23 @@ Track = function (trackId){
 
 Rotation = function(tracks) {
     var currentTrack = tracks[1];
-
     this.currentTrack = function() {
         return currentTrack;
     };
-
     this.nextTrack = function () {
-        var currentIndex = tracks.indexOf(currentTrack);
-        var nextTrackIndex = (currentIndex + 1) % tracks.length;
-        var nextTrackId = tracks[nextTrackIndex];
+        var currentIndex = tracks.indexOf(currentTrack),
+        	nextTrackIndex = (currentIndex + 1) % tracks.length,
+        	nextTrackId = tracks[nextTrackIndex];
         currentTrack = nextTrackId;
         return currentTrack
     };
-	
     this.prevTrack = function () {
-        var currentIndex = tracks.indexOf(currentTrack);
-        var prevTrackIndex = (currentIndex - 1);
+        var currentIndex = tracks.indexOf(currentTrack),
+        	prevTrackIndex = (currentIndex - 1),
+			prevTrackId = tracks[prevTrackIndex];
 		if(prevTrackIndex<0){
 			prevTrackIndex=(tracks.length)-1;
 		}
-        var prevTrackId = tracks[prevTrackIndex];
         currentTrack = prevTrackId;
         return currentTrack
     };
@@ -119,11 +109,10 @@ function changeTitle(newTrack, oldTrack){
 			$('#title').html(titleHtml).fadeIn(500);
 		});
 	}
-	else{
+	else{ // If changing tracks, fade the old one out first
 		$('#title').html(titleHtml).fadeIn(500);
 	}
 }
-
 
 function getPhotos(){
 	$.get('https://api.instagram.com/v1/users/252833323/media/recent/?client_id=027d4ba8024541bda9174d50c1592dfa', function(images){
@@ -134,24 +123,23 @@ function getPhotos(){
 
 function playSongs(){
 	SC.get('/users/11021442/tracks', function(songs){
-		var rotation = new Rotation(songs);
-		var currentTrack = rotation.currentTrack();
-		changeTitle(currentTrack, undefined);
-		var currentPlayingTrack = new Track(currentTrack.uri);
-		var $soundcloud = $('#soundcloud');
-		var controlSize;
+		var rotation = new Rotation(songs),
+			currentTrack = rotation.currentTrack(),
+			currentPlayingTrack = new Track(currentTrack.uri),
+			$soundcloud = $('#soundcloud'),
+			controlSize = ($(window).width() <= 700) ? '.small' : '.large',
+			nextRotation = 0,
+			prevRotation = 0;		
+		changeTitle(currentTrack);
 		$('.play').on('click', function(e){
 			e.preventDefault();
-			controlSize = ($(window).width() <= 700) ? '.small' : '.large';
 			if($soundcloud.hasClass('paused')){
-				$($soundcloud).removeClass('paused');
-				$soundcloud.addClass('playing');
+				$soundcloud.removeClass('paused').addClass('playing');
 				currentPlayingTrack.pause();
 				$(controlSize+'.pause').show();
 				$(controlSize+'.play').hide();
 			}
 			else{
-				controlSize = ($(window).width() <= 700) ? '.small' : '.large';
 				$soundcloud.addClass('playing');
 				currentPlayingTrack.play();
 				ga('send', 'event', 'Soundcloud', 'Play', currentTrack.title);
@@ -159,17 +147,13 @@ function playSongs(){
 				$(controlSize+'.play').hide();
 			}
 		});
-
 		$('.pause').on('click', function(e){
 			e.preventDefault();
 			currentPlayingTrack.pause();
-			controlSize = ($(window).width() <= 700) ? '.small' : '.large';
-			$soundcloud.addClass('paused');
-			$soundcloud.removeClass('playing');
+			$soundcloud.addClass('paused').removeClass('playing');
 			$(controlSize+'.pause').hide();
 			$(controlSize+'.play').show();
 		});
-		var nextRotation = 0;
 		$('.nextSong').on('click', function(e){
 			e.preventDefault();
 			currentPlayingTrack.stop();
@@ -183,7 +167,6 @@ function playSongs(){
 			}
 			$soundcloud.removeClass('paused');
 		});
-		var prevRotation = 0;
 		$('.prevSong').on('click', function(e){
 			e.preventDefault();
 			currentPlayingTrack.stop();
@@ -198,12 +181,13 @@ function playSongs(){
 			$soundcloud.removeClass('paused');
 		});
 	});
+	playerStyle(); // Fix the style of the song player after it enters
 }
 
 function addPhotos(photos){
 	$('#photoContainer').append('<a href="' + photos[0].link + '" onClick="ga(\'send\', \'event\',  \'Instagram\', &#39;' + photos[0].link + '&#39;);\">' + '<img id="slide0" src="' + photos[0].images.low_resolution.url + '">' + '</a>');
 	$('#slide0').addClass('active');
-	for(i=1;i<photos.length;i++){
+	for(var i=1;i<photos.length;i++){
 		photoUrl = photos[i].images.low_resolution.url;
 		photoLink = photos[i].link;
 		$('#photoContainer').append('<a href="' + photoLink + '" onClick="ga(\'send\', \'event\', \'Instagram\', &#39;' + photoLink + '&#39;);>\">' + '<img id="slide' + (i) + '" src="' + photoUrl + '">' + '</a>');
@@ -213,8 +197,8 @@ function addPhotos(photos){
 function rotatePhotos(){
 	$('.nextPhoto').click(function(e){
 		e.preventDefault();
-		var $active = $('#photoContainer a img.active');
-		var $next = ($active.parent().next().length > 0) ? $active.parent().next().contents() : $('#photoContainer a:first img');
+		var $active = $('#photoContainer a img.active'),
+			$next = ($active.parent().next().length > 0) ? $active.parent().next().contents() : $('#photoContainer a:first img');
 		$next.css('z-index',2);
 		$active.fadeOut(750,function(){
 			$active.css('z-index',1).show().removeClass('active');
@@ -223,8 +207,8 @@ function rotatePhotos(){
 	});
 	$('.prevPhoto').click(function(e){
 		e.preventDefault();
-		var $active = $('#photoContainer a img.active');
-		var $prev = ($active.parent().prev().length > 0) ? $active.parent().prev().contents() : $('#photoContainer a:last img');
+		var $active = $('#photoContainer a img.active'),
+			$prev = ($active.parent().prev().length > 0) ? $active.parent().prev().contents() : $('#photoContainer a:last img');
 		$prev.css('z-index',2);
 		$active.fadeOut(750,function(){
 			$active.css('z-index',1).show().removeClass('active');
@@ -235,14 +219,13 @@ function rotatePhotos(){
 
 function showDates(){
 	var printed = false,
-	upcomingShows = 0;
-	
-	$('#soundcloud').watch('data-scroll-reveal', function(){	
-		if(($(this).attr('data-scroll-reveal-complete')) == "true" && !printed){	
+		upcomingShows = 0;
+	$('#live').watch('opacity', function(){	
+		if(($(this).css('opacity')) == 1 && !printed){	
 			$.getJSON('shows.json')
 			.done(function(data){
 				typewriter("shows_headline", "Upcoming Shows", 150);
-				for(i=0;i<data.shows.length;i++){
+				for(var i=0;i<data.shows.length;i++){
 					if(isUpcoming(data.shows[i])){
 						upcomingShows++;
 						if(data.shows[i].url)
@@ -255,7 +238,7 @@ function showDates(){
 					}
 				}
 				if(upcomingShows === 0){
-					$("#upcomingDates ul").append("<li>" + "Shows TBD ... Check back soon ...");
+					$("#upcomingDates ul").append("<li>" + "Shows TBD ... Check back soon ..." + "</li>");
 				}
 			})
 			.fail(function (jqxhr, textStatus, error ) {
@@ -268,10 +251,12 @@ function showDates(){
 }
 
 function isUpcoming(show){
-	var showDate = Date.parse(show.date);
-	var comparison = Date.today().compareTo(showDate);
-	if(comparison != 1){return true;}
-	else{return false;}
+	var showDate = Date.parse(show.date),
+		comparison = Date.today().compareTo(showDate);
+	if(comparison != 1)
+		{return true;}
+	else
+		{return false;}
 }
 
 function playerStyle(){
@@ -280,12 +265,11 @@ function playerStyle(){
 			$(this).css({"opacity":".85", "transition": "opacity .6s", "-webkit-transition": "opacity .6s"});
 		}
 	})
-	
 }
 
 function typewriter(element, string, speed){
 	var index = 0;
-	var intObject = setInterval(function() {
+	var	intObject = setInterval(function() {
 		document.getElementById(element).innerHTML+=string[index];
 		index++;
 		if(index==string.length){
@@ -293,5 +277,3 @@ function typewriter(element, string, speed){
 		}
 	}, speed);
 }
-
-
